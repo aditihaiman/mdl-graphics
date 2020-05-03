875fd92a8ed38a9a0141bd0af2c1a474ec0bb444
aditihaiman <ahaiman00@stuy.edu>
2020-05-03 13:57:01 -0400

    first try

--- script.py
+++ script.py
@@ -28,7 +28,7 @@ def run(filename):
               255,
               255]]
 
-    color = [0, 0, 0]
+    color = [255, 0, 0]
     tmp = new_matrix()
     ident( tmp )
 
@@ -36,6 +36,7 @@ def run(filename):
     screen = new_screen()
     zbuffer = new_zbuffer()
     tmp = []
+    polygons = []
     step_3d = 100
     consts = ''
     coords = []
@@ -48,4 +49,54 @@ def run(filename):
 
     print(symbols)
     for command in commands:
-        print(command)
+        c = command['op']
+        args = command['args']
+        
+        if c == "push":
+            stack.append( [x[:] for x in stack[-1]] )
+        if c == "pop":
+            stack.pop()
+        if c == "move":
+            tmp = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+        if c == "rotate":
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                tmp = make_rotX(theta)
+            elif args[0] == 'y':
+                tmp = make_rotY(theta)
+            else:
+                tmp = make_rotZ(theta)
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+        if c == "scale":
+            tmp = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+        if c == "sphere":
+            add_sphere(polygons,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+            matrix_mult( stack[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
+        if c == "box":
+            add_box(polygons,
+                    float(args[0]), float(args[1]), float(args[2]),
+                    float(args[3]), float(args[4]), float(args[5]))
+            matrix_mult( stack[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
+        if c == "torus":
+            add_torus(polygons,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), step_3d)
+            matrix_mult( stack[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
+        if c == "display":
+            display(screen)
+        if c == "save":
+            save_extension(screen, args[0])
+        print("C", command['op'], command)
